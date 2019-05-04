92b09a83f33746f61bc2a15a36848dfc7ca958d6
Ealse <ealsebouma@gmail.com>
2019-05-04 19:21:51 +0200

    Public partial class Welcome removed from class User

--- DotNetStandardApi/Entities/User.cs
+++ DotNetStandardApi/Entities/User.cs
@@ -7,28 +7,25 @@ namespace KoenZomers.Tado.Api.Entities
     /// </summary>
     public class User
     {
-        public partial class Welcome
-        {
-            [JsonProperty("name")]
-            public string Name { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
 
-            [JsonProperty("email")]
-            public string Email { get; set; }
+        [JsonProperty("email")]
+        public string Email { get; set; }
 
-            [JsonProperty("username")]
-            public string Username { get; set; }
+        [JsonProperty("username")]
+        public string Username { get; set; }
 
-            [JsonProperty("id")]
-            public string Id { get; set; }
+        [JsonProperty("id")]
+        public string Id { get; set; }
 
-            [JsonProperty("homes")]
-            public Home[] Homes { get; set; }
+        [JsonProperty("homes")]
+        public Home[] Homes { get; set; }
 
-            [JsonProperty("locale")]
-            public string Locale { get; set; }
+        [JsonProperty("locale")]
+        public string Locale { get; set; }
 
-            [JsonProperty("mobileDevices")]
-            public MobileDevice.Item[] MobileDevices { get; set; }
-        }
+        [JsonProperty("mobileDevices")]
+        public MobileDevice.Item[] MobileDevices { get; set; }
     }
 }
