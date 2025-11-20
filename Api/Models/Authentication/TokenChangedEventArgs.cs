d7033ca469156ae93f9a254c1b58512a78cf4549
NMauel <info@n-coded.nl>
2025-11-20 13:16:12 +0100

    Extended Tado class with TokenChangedEvent to be able to retrieve a refreshed token.

---
+++ Api/Models/Authentication/TokenChangedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace KoenZomers.Tado.Api.Models.Authentication;
+
+/// <summary>
+/// EventArgs to pass the new token when it has been changed/refreshed
+/// </summary>
+public sealed class TokenChangedEventArgs(Token token) : EventArgs
+{
+    /// <summary>
+    /// The new token
+    /// </summary>
+    public Token Token { get; } = token;
+}
